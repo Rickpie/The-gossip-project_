3092763c8afd341330234c07c7c35aff4338d9bc
MaximeGillot5 <mgillot56@icloud.com>
2023-02-15 16:57:46 +0100

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  resources :gossip
+  resources :user
+  resources :city
+  #get 'static_pages/contact'
+  #get 'static_pages/team'
+  get 'contact', to: 'static_pages#contact'
+  get 'team', to: 'static_pages#team'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root 'static_pages#home'
+end
